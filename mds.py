a3f40e5c727e97717576cb108db91e01393defe4
Xacla <area184persona@gmail.com>
2019-04-26 18:36:41 +0900

    修正

--- mds.py
+++ mds.py
@@ -7,7 +7,10 @@ def d_square_matrix_function(x):
     x_t=x.T
     x_matrix=np.dot(x_t,x)
     ones_matrix=np.ones((x_matrix.shape[0],x_matrix.shape[1]))
-    d_square=np.dot(x_matrix,ones_matrix) -2*x_matrix+ np.dot(ones_matrix,x_matrix)
+    x_lambda,b=np.linalg.eig(x_matrix)
+    print(x_lambda)
+    x_lambda=np.diag(x_lambda)
+    d_square=np.dot(x_lambda,ones_matrix) -2*x_matrix+ np.dot(ones_matrix,x_lambda)
     return d_square
 
 def centering_matrix(n):
@@ -32,6 +35,7 @@ d_square=d_square_matrix_function(data)
 p=p_matrix(d_square)
 
 #%%
+# 特異値分解は力尽きた
 u,s,a= np.linalg.svd(p, full_matrices=True)
 
 plt.scatter(a[:,0],a[:,1])
